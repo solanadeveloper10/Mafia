3ec1900e9f96ddb65091ccbafdefe417fc0255c2
solanadeveloper10 <solanadeveloper10@gmail.com>
2024-11-19 10:14:37 +0200

    8

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('copy-button').addEventListener('click', function () {
-    const contractText = document.getElementById('contract-text').textContent;
-    navigator.clipboard.writeText(contractText).then(() => {
-      alert('Contract copied to clipboard!');
-    }).catch(err => {
-      console.error('Error copying contract: ', err);
-    });
-  });
